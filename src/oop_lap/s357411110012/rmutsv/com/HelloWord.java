43a57a09831b0c0bda92f59975e96808d0afdcfb
sirintrajern <bobow159@gmail.com>
2017-11-19 15:18:54 +0700

    Add information

--- src/oop_lap/s357411110012/rmutsv/com/HelloWord.java
+++ src/oop_lap/s357411110012/rmutsv/com/HelloWord.java
@@ -4,7 +4,12 @@ public class HelloWord {
 // create main method
 public static void main(String[] args) {
   System.out.println("Hello World,I'am JERN");
-    
+  System.out.println("sirintra choatthipipatworakul");
+  System.out.println("Information System");
+  System.out.println("MT.RMUTSV");
+
+
+
 
 
 
