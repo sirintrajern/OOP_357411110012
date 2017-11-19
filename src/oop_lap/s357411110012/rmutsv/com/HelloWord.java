613908b093bf20149a4d1b16cc893aaa746d947b
sirintrajern <bobow159@gmail.com>
2017-11-19 14:49:28 +0700

    Testing upload to githab

---
+++ src/oop_lap/s357411110012/rmutsv/com/HelloWord.java
@@ -0,0 +1,14 @@
+package oop_lap.s357411110012.rmutsv.com;
+
+public class HelloWord {
+// create main method
+public static void main(String[] args) {
+  System.out.println("Hello World,I'am JERN");
+    
+
+
+
+}//main
+
+
+}//class
