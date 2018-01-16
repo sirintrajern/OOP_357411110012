038b99993b847029200ecb79a2581cd84d1d977d
sirintrajern <sirintrajern@gmail.com>
2018-01-16 11:29:31 +0700

    midterm

---
+++ src/OOP_lab3/grade.java
@@ -0,0 +1,13 @@
+package OOP_lab3;
+
+import java.util.Ccanner;
+// score 0-49 grade F
+// score 50-59 grade D
+// score 60-69 grade C
+// score 70-79 grade B
+// score 80-100 grade A
+public class grade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner ()
+    }
+}
