c1f3d8ee09f7c0c6a6aa28558cb2dab923c5408e
sirintrajern <sirintrajern@gmail.com>
2018-01-30 10:21:27 +0700

    PersonInfo

---
+++ src/OOP_lab8/Personinfo.java
@@ -0,0 +1,10 @@
+package OOP_lab8;
+
+public class Personinfo {
+    public static void main(String[] args) {
+        student student = new student("1111111111111",
+                "Boy Saiyai","Male",20,"20222222222222","IS Manament Tectnology");
+       System.out.println (student.toString());
+       System.out.println(student.getName());
+    }
+}
