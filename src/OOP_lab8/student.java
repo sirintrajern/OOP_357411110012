91cd8c88cd6f93cf560324042329edc1fe7c4948
sirintrajern <sirintrajern@gmail.com>
2018-01-30 09:56:34 +0700

    Person and student

---
+++ src/OOP_lab8/student.java
@@ -0,0 +1,34 @@
+package OOP_lab8;
+
+//person is superclass
+//student is subclass
+
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+public class student extends Person {
+    private String studentID;
+    private String major;
+
+
+    public student(String personID, String name, String gender, int age,String studentID,String major) {
+        super(personID, name, gender, age);
+        this.studentID = studentID;
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return "student{" +
+                "studentID='" + studentID + '\'' +
+                ", major='" + major + '\'' +
+                "} " + super.toString();
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+}//class
